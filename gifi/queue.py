83c430ed8959b56c7d18db0d5ba07aef724e98b4
Grzegorz Kokosiński <g.kokosinski@gmail.com>
2015-07-06 14:36:43 +0200

    queue - minor fixes

--- gifi/queue.py
+++ gifi/queue.py
@@ -1,10 +1,12 @@
-from command import Command, AggregatedCommand
+from command import Command, AggregatedCommand, CommandException
 from internal.git_utils import get_repo, check_repo_is_clean
 
 
 def _pop():
     repo = get_repo()
     check_repo_is_clean(repo)
+    if repo.git.stash('list') == '':
+        raise CommandException('There is nothing in the queue to pop from.')
     repo.git.stash('apply', '--index')
     _pop_finish()
 
@@ -12,6 +14,7 @@ def _pop():
 def _pop_finish(repo=None):
     repo = get_repo(repo)
     commit_message = repo.git.stash('list', '--max-count=1', '--oneline')
+    commit_message = ''.join(commit_message.split(': ')[2:])
     repo.index.commit(_unescape_new_lines(commit_message))
     repo.git.stash('drop')
 
@@ -19,6 +22,8 @@ def _pop_finish(repo=None):
 def _push():
     repo = get_repo()
     check_repo_is_clean(repo)
+    if repo.head.commit == repo.commit('origin/master'):
+        raise CommandException('You are currently at origin/master, there is nothing to push')
     commit_message = repo.head.commit.message
     repo.git.reset('--soft', 'HEAD^')
     repo.git.stash('save', _escape_new_lines(commit_message))
